a1f0622eb27a4afc4a8bda2b463877e1a84558d6
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-02 21:48:51 +0500

    GoldAppraiser skeleton with all 5 visit stubs

--- src/com/narxoz/rpg/visitor/CurseDetector.java
+++ src/com/narxoz/rpg/visitor/CurseDetector.java
@@ -2,9 +2,29 @@ package com.narxoz.rpg.visitor;
 
 import com.narxoz.rpg.artifact.*;
 
-import java.util.ArrayList;
-import java.util.List;
+public class GoldAppraiser implements ArtifactVisitor {
 
-public class CurseDetector {
+    private int totalValue = 0;
+
+    public int getTotalValue() { return totalValue; }
+
+    @Override
+    public void visit(Weapon weapon) {
+    }
+
+    @Override
+    public void visit(Potion potion) {
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+    }
+
+    @Override
+    public void visit(Ring ring) {
+    }
+
+    @Override
+    public void visit(Armor armor) {
+    }
 }
-
