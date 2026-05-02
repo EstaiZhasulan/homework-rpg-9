5f75cc16dd66bc2917ddbb0bb1e19e873cf5a218
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-02 22:06:51 +0500

    implement GoldAppraiser visit(Weapon) and visit(Armor)

--- src/com/narxoz/rpg/visitor/GoldAppraiser.java
+++ src/com/narxoz/rpg/visitor/GoldAppraiser.java
@@ -1,4 +1,39 @@
 package com.narxoz.rpg.visitor;
 
-public class GoldAppraiser {
+import com.narxoz.rpg.artifact.*;
+
+public class GoldAppraiser implements ArtifactVisitor {
+
+    private int totalValue = 0;
+
+    public int getTotalValue() { return totalValue; }
+
+    @Override
+    public void visit(Weapon weapon) {
+        int appraisal = (int)(weapon.getValue() * 1.5 + weapon.getAttackBonus() * 10);
+        totalValue += appraisal;
+        System.out.printf("  [GoldAppraiser] %-22s -> %4d gold  (weapon ×1.5 + ATK bonus)%n",
+                weapon.getName(), appraisal);
+    }
+
+    @Override
+    public void visit(Potion potion) {
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+    }
+
+    @Override
+    public void visit(Ring ring) {
+    }
+
+    @Override
+    public void visit(Armor armor) {
+        int appraisal = (int)(armor.getValue() * 1.3 + armor.getDefenseBonus() * 8);
+        totalValue += appraisal;
+        System.out.printf("  [GoldAppraiser] %-22s -> %4d gold  (armor ×1.3 + DEF bonus)%n",
+                armor.getName(), appraisal);
+    }
+
 }
